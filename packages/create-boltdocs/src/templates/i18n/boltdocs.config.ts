01e0af890b9b7ce3c6ad8a0cd8b10fd467a3613d
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-05-13 00:26:36 -0400

    fix(create-boltdocs): updated layouts

--- packages/create-boltdocs/src/templates/i18n/boltdocs.config.ts
+++ packages/create-boltdocs/src/templates/i18n/boltdocs.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   theme: {
     title: '{{title}}',
     description: 'Documentation for my project',
-    breadcrumbs: true,
     codeTheme: {
       light: 'github-light',
       dark: 'github-dark',
