7f9a871af80ebdee1f438fd95a4ae23af4bf31d7
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-05-13 00:05:04 -0400

    refactor: added import `mdx_component_default`  and removed components old

--- packages/core/src/client/app/mdx-component.tsx
+++ packages/core/src/client/app/mdx-component.tsx
@@ -1,72 +1,8 @@
-import { Link as LucideLink } from 'lucide-react'
-import * as MdxComponents from '../components/mdx'
-import { Link } from '../components/primitives/link'
-
-const Anchor = ({
-  href,
-  children,
-  ...props
-}: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
-  return (
-    <Link href={href || ''} {...props}>
-      {children}
-    </Link>
-  )
-}
-
-const Heading = ({
-  level,
-  id,
-  children,
-  ...props
-}: {
-  level: number
-  id?: string
-  children?: React.ReactNode
-} & React.HTMLAttributes<HTMLHeadingElement>) => {
-  const Tag = `h${level}` as any
-  return (
-    <Tag id={id} {...props} className="boltdocs-heading">
-      {children}
-      {id && (
-        <Link href={`#${id}`} className="header-anchor" aria-label="Anchor">
-          <LucideLink size={16} />
-        </Link>
-      )}
-    </Tag>
-  )
-}
-
-import { Loading } from '../components/ui-base/loading'
 import { NotFound } from '../components/ui-base/not-found'
+import { mdx_components_default } from '../components/mdx'
 
 export const mdxComponentsDefault = {
-  ...MdxComponents,
-  Loading,
+  ...mdx_components_default,
   NotFound,
   '404': NotFound,
-  a: Anchor,
-  h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <Heading level={1} {...props} />
-  ),
-  h2: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <Heading level={2} {...props} />
-  ),
-  h3: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <Heading level={3} {...props} />
-  ),
-  h4: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <Heading level={4} {...props} />
-  ),
-  h5: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <Heading level={5} {...props} />
-  ),
-  h6: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <Heading level={6} {...props} />
-  ),
-  pre: (props: React.HTMLAttributes<HTMLPreElement>) => (
-    <MdxComponents.CodeBlock {...props}>
-      {props.children}
-    </MdxComponents.CodeBlock>
-  ),
 }
