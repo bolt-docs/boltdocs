a2420cf999e5a23bc9bc1a2ec6bf7ab1d43d6ac1
Jesus Alcala <jesusalcalarojas@gmail.com>
2026-05-13 00:20:19 -0400

    feat(core): added index of export `mdx_components_default`

---
+++ packages/core/src/client/components/mdx/index.ts
@@ -0,0 +1,15 @@
+import { Field } from './field'
+import { Typographics } from './typographics'
+import { TableComponents } from './table'
+import { Callout } from './callout'
+import { CodeBlock } from './code-block'
+import { ImageComponents } from './image'
+
+export const mdx_components_default = {
+  ...Typographics,
+  ...TableComponents,
+  ...ImageComponents,
+  pre: CodeBlock,
+  Field,
+  Callout,
+}
